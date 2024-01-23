8abb3906ae184f4a86114a46a7127d7acb18ef99
seriymalta <milto_sr@mail.ru>
2024-01-23 13:09:23 +0000

    Added tests for inequality

--- 02_basic_python.py
+++ 02_basic_python.py
@@ -1,8 +1,8 @@
 #Basic arithmetic operations in Python
 
 #Add two numbers
-print(2+4)
-print(type(10/5))
+print(2 + 4)
+print(type(10 / 5))
 
 #Exponentiation
 print(2**3)
@@ -12,15 +12,15 @@ y = x**2
 print(y)
 
 #String
-a = "Hello, how're you?" #equivalently: a = "Hello"
+a = "Hello, how're you?"  #equivalently: a = "Hello"
 
 #Arithmetic operations on strings
 #Multiplication
-print(a*5)
+print(a * 5)
 
 #Concatenation
 b = "I'm good, thank you"
-print (a+ " " + b)
+print(a + " " + b)
 
 #Indexing and slicing
 #First element
@@ -29,21 +29,25 @@ print([0])
 #Len
 print(f'Our string a has {len(a)} characters')
 
-print ('Our string a has {} characters'.format(len(a)))
+print('Our string a has {} characters'.format(len(a)))
 
 #Logical statements
-print(2==2)
+print(2 == 2)
 
-print(2 == 3) #False = equal to 0
+print(2 == 3)  #False = equal to 0
 
-print (True==1) #Checking whether True is equivalent to 1
+print(True == 1)  #Checking whether True is equivalent to 1
 
 #True evaluates to 1 and False evaluates to 0
 
 print(True + True + True)
 
-print (a==b)
+print(a == b)
 
 #Multiple logical statements
 
 print(2 == 2 and 3 == 3)
+
+print(2 != 3)
+
+print(2<3)
